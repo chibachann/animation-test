df8f3fb65e7c9c16481551bc0969260badb27b54
chiba <ryotai2677@gmail.com>
2024-11-02 15:09:56 +0900

    a

---
+++ src/pages/lab/TopPage01.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Top01 from "../../components/Top01/Top01";
+
+const TopPage01 = () => {
+    return (
+        <Top01 />
+    );
+}
+
+export default TopPage01;
---
+++ src/pages/lab/TopPage02.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Top02 from "../../components/Top02/Top02";
+
+const TopPage02 = () => {
+    return (
+        <Top02 />
+    );
+}
+
+export default TopPage02;
