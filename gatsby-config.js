3e0fc8f56fd462c48b7086912a67716edf37692a
chiba <ryotai2677@gmail.com>
2024-11-03 02:32:30 +0900

    a

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
     siteMetadata: {
         title: 'animation-test',
         description: "anime test",
+        siteUrl: "https://www.grid-practice02.com",
         author: "chiba",
         lang: "ja",
     },
